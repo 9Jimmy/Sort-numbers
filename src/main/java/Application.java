877b4c940fbebc63d50724e26e6496e37bb09c77
9Jimmy <9jimmyw@gmail.com>
2019-03-11 18:20:22 +0500

    sorting-files-v2

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,18 +1,29 @@
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Application {
+ class Application {
     public static void main(String[] args) {
+        System.out.print("When you finish your work enter \'exit\'\n");
+
         Sorting sorting = new Sorting();
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Please enter the path of text file you want to sort (use '\\\\' instead of '\\')");
-        String path = scanner.nextLine();
+        boolean session = true;
 
-        try {
-            sorting.sortingNumbers(path);
-        } catch (IOException e) {
-            e.printStackTrace();
+        while (session) {
+            System.out.print("============================================");
+            System.out.print("\nEnter the path of text file you want to sort\n");
+            String input = scanner.nextLine().replace("\\", "\\\\");
+
+            try {
+                sorting.sortingNumbers(input);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if(input.equals("exit")){
+                session = false;
+            }
         }
     }
 }
--- src/main/java/Sorting.java
+++ src/main/java/Sorting.java
@@ -2,36 +2,54 @@ import java.io.*;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class Sorting implements Serializable {
-    public void sortingNumbers(String path) throws IOException {
+class Sorting implements Serializable {
+
+    int sortingNumbers(String path) throws IOException {
         Set<Integer> set = new TreeSet<>();
 
-        try(BufferedReader br = new BufferedReader(new FileReader(path))){
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             StringBuilder sb = new StringBuilder();
             String line = br.readLine();
 
-            while(line!=null){
+            while (line != null) {
                 sb.append(line);
                 sb.append(System.lineSeparator());
                 line = br.readLine();
             }
             String everything = sb.toString();
-            for(String x : everything.split("\\s")){
-                try{
+
+            boolean process = true;
+
+            for (String x : everything.split("\\s")) {
+                if (x.equals("-")) {
+                    System.out.print("\nFile already sorted!\n");
+                    process = false;
+                }
+                try {
                     set.add(Integer.parseInt(x));
-                } catch (NumberFormatException e){
+                } catch (NumberFormatException e) {
                     continue;
                 }
             }
-            System.out.print("Not sorted: ");
+
+            if(process==false){
+                return 0;
+            }
+
+            System.out.print("\nNot sorted: ");
             System.out.println(everything);
             System.out.print("Sorted: ");
             System.out.println(set);
 
-            try(FileWriter writer = new FileWriter(path, false)){
-                writer.write("Before: " + everything + "\n");
+            try (FileWriter writer = new FileWriter(path, false)) {
+                writer.write("-\nBefore: " + everything + "\n");
                 writer.write("After: " + set);
             }
         }
+        catch(FileNotFoundException e) {
+            if (!path.equals("exit"))
+                System.out.printf("\nFile at \'%s\' not found.\n", path);
+        }
+        return 0;
     }
 }
